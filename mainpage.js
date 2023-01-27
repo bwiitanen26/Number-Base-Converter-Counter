5919fa9012f4c351fb9d962e41cff52da0febb04
bwiitanen26 <bwiitanen26@gmail.com>
2023-01-26 19:44:23 -0800

    beginning

--- mainpage.js
+++ mainpage.js
@@ -2,4 +2,29 @@ function toTextConverter() {
     window.location.href = "textconverter.html"
 }
 
+function updateFromDec() {
+    document.getElementById("binField").value = document.getElementById("decField").value;
+    document.getElementById("decField").value = document.getElementById("decField").value;
+    document.getElementById("octField").value = document.getElementById("decField").value;
+    document.getElementById("hexField").value = document.getElementById("decField").value;
+    document.getElementById("asciiField").value = document.getElementById("decField").value;
+}
+
+function updateFromBin() {
+    document.getElementById("binField").value = document.getElementById("decField").value;
+}
+
+function updateFromOct() {
+    document.getElementById("binField").value = document.getElementById("decField").value;
+}
+
+function updateFromHex() {
+    document.getElementById("binField").value = document.getElementById("decField").value;
+}
+
+function updateFromASCII() {
+    document.getElementById("binField").value = document.getElementById("decField").value;
+}
+
 document.getElementById("toTextConButton").addEventListener("click", toTextConverter);
+document.getElementById("decField").addEventListener("keyup", updateFromDec);
