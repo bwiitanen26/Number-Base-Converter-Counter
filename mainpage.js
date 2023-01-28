52c2a9ee0120afd412ecd739dc5aa997597e03a7
bwiitanen26 <bwiitanen26@gmail.com>
2023-01-28 10:30:08 -0800

    more skeleton

--- mainpage.js
+++ mainpage.js
@@ -3,41 +3,129 @@ function toTextConverter() {
 }
 
 function updateFromDec() {
+    //Constraints (only allow digits 0-9)
+    const decval = document.getElementById("decField").value;
+
+    
+
+
+
+    //Update Binary Field
     document.getElementById("binField").value = document.getElementById("decField").value;
 
 
-
+    //Update Octal Field
     document.getElementById("octField").value = document.getElementById("decField").value;
 
 
-
+    //Update Hex Field
     document.getElementById("hexField").value = document.getElementById("decField").value;
 
 
-
+    //Update ASCII Field
     document.getElementById("asciiField").value = document.getElementById("decField").value;
-
-
-
 
 
 }
 
 function updateFromBin() {
-    document.getElementById("binField").value = document.getElementById("decField").value;
+    //Constraints (only allow digits 0-1)
+
+
+
+
+    //Update Decimal Field
+    document.getElementById("decField").value = document.getElementById("binField").value;
+
+
+    //Update Octal Field
+    document.getElementById("octField").value = document.getElementById("binField").value;
+
+
+    //Update Hex Field
+    document.getElementById("hexField").value = document.getElementById("binField").value;
+
+
+    //Update ASCII Field
+    document.getElementById("asciiField").value = document.getElementById("binField").value;
+
+
 }
 
 function updateFromOct() {
-    document.getElementById("binField").value = document.getElementById("decField").value;
+    //Constraints (only allow digits 1-8)
+
+
+
+    //Update Decimal Field
+    document.getElementById("decField").value = document.getElementById("octField").value;
+
+
+    //Update Binary Field
+    document.getElementById("binField").value = document.getElementById("octField").value;
+
+
+    //Update Hex Field
+    document.getElementById("hexField").value = document.getElementById("octField").value;
+
+
+    //Update ASCII Field
+    document.getElementById("asciiField").value = document.getElementById("octField").value;
+
+
 }
 
 function updateFromHex() {
-    document.getElementById("binField").value = document.getElementById("decField").value;
+    //Constraints (only characters 0-F)
+
+
+
+
+    //Update Decimal Field
+    document.getElementById("decField").value = document.getElementById("hexField").value;
+
+
+    //Update Binary Field
+    document.getElementById("binField").value = document.getElementById("hexField").value;
+
+
+    //Update Octal Field
+    document.getElementById("octField").value = document.getElementById("hexField").value;
+
+
+    //Update ASCII Field
+    document.getElementById("asciiField").value = document.getElementById("hexField").value;
+
+
 }
 
 function updateFromASCII() {
-    document.getElementById("binField").value = document.getElementById("decField").value;
+    //Constraints (only allow input to be 1 single legal character in ASCII table)
+
+
+
+
+    //Update Decimal Field
+    document.getElementById("decField").value = document.getElementById("asciiField").value;
+
+
+    //Update Binary Field
+    document.getElementById("binField").value = document.getElementById("asciiField").value;
+
+
+    //Update Octal Field
+    document.getElementById("octField").value = document.getElementById("asciiField").value;
+
+
+    //Update Hex Field
+    document.getElementById("hexField").value = document.getElementById("asciiField").value;
+
+
 }
 
 document.getElementById("toTextConButton").addEventListener("click", toTextConverter);
-document.getElementById("decField").addEventListener("blur", updateFromDec);
+document.getElementById("decField").addEventListener("keyup", updateFromDec);
+document.getElementById("binField").addEventListener("keyup", updateFromBin);
+document.getElementById("octField").addEventListener("keyup", updateFromOct);
+document.getElementById("hexField").addEventListener("keyup", updateFromHex);
+document.getElementById("asciiField").addEventListener("keyup", updateFromASCII);
