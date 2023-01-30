dea0e307918a0e737082aef904e99197ff5dadee
Brandon Wiitanen <47845999+bwiitanen26@users.noreply.github.com>
2023-01-30 01:04:37 +0000

    Conversions besides ASCII

--- mainpage.js
+++ mainpage.js
@@ -1,126 +1,110 @@
+//Function that switches screen to text converter page
 function toTextConverter() {
-    window.location.href = "textconverter.html"
+  window.location.href = "textconverter.html"
 }
-
+  
 function updateFromDec() {
-    //Constraints (only allow digits 0-9)
-    const decval = document.getElementById("decField").value;
+  //Constraint (only allow digits 0-9)
+  let decval = document.getElementById("decField").value.replace(/[^0-9]/, "");
+  document.getElementById("decField").value = decval;
 
-    
+  //Update Binary Field
+  document.getElementById("binField").value = document.getElementById("decField").value.length > 0 ? Number(decval).toString(2) : "";
 
+  //Update Octal Field
+  document.getElementById("octField").value = document.getElementById("decField").value.length > 0 ? Number(decval).toString(8) : "";
 
+  //Update Hex Field
+  document.getElementById("hexField").value = document.getElementById("decField").value.length > 0 ? Number(decval).toString(16) : "";
 
-    //Update Binary Field
-    document.getElementById("binField").value = document.getElementById("decField").value;
-
-
-    //Update Octal Field
-    document.getElementById("octField").value = document.getElementById("decField").value;
-
-
-    //Update Hex Field
-    document.getElementById("hexField").value = document.getElementById("decField").value;
-
-
-    //Update ASCII Field
-    document.getElementById("asciiField").value = document.getElementById("decField").value;
-
-
+  //Update ASCII Field
+  document.getElementById("asciiField").value = document.getElementById("decField").value.length > 0 ? String.fromCharCode(Number(document.getElementById("decField").value)) : "";
 }
 
 function updateFromBin() {
-    //Constraints (only allow digits 0-1)
+  //Constraint (only allow digits 0-1)
+  let binval = document.getElementById("binField").value.replace(/[^0-1]/, "");
+  document.getElementById("binField").value = binval;
 
+  //Update Decimal Field
+  let decval = parseInt(binval,2);
+  document.getElementById("decField").value = document.getElementById("binField").value.length > 0 ? decval : "";
 
+  //Update Octal Field
+  document.getElementById("octField").value = document.getElementById("decField").value.length > 0 ? Number(decval).toString(8) : "";
 
+  //Update Hex Field
+  document.getElementById("hexField").value = document.getElementById("decField").value.length > 0 ? Number(decval).toString(16) : "";
 
-    //Update Decimal Field
-    document.getElementById("decField").value = document.getElementById("binField").value;
-
-
-    //Update Octal Field
-    document.getElementById("octField").value = document.getElementById("binField").value;
-
-
-    //Update Hex Field
-    document.getElementById("hexField").value = document.getElementById("binField").value;
-
-
-    //Update ASCII Field
-    document.getElementById("asciiField").value = document.getElementById("binField").value;
-
-
+  //Update ASCII Field
+  document.getElementById("asciiField").value = document.getElementById("decField").value.length > 0 ? String.fromCharCode(Number(document.getElementById("decField").value)) : "";
 }
 
 function updateFromOct() {
-    //Constraints (only allow digits 1-8)
+  //Constraint (only allow digits 1-8)
+  let octval = document.getElementById("octField").value.replace(/[^0-8]/, "")
+  document.getElementById("octField").value = octval;
 
+  //Update Decimal Field
+  let decval = parseInt(octval,8);
+  document.getElementById("decField").value = document.getElementById("octField").value.length > 0 ? decval : "";
+  
+  //Update Binary Field
+  document.getElementById("binField").value = document.getElementById("decField").value.length > 0 ? Number(decval).toString(2) : "";
 
+  //Update Hex Field
+  document.getElementById("hexField").value = document.getElementById("decField").value.length > 0 ? Number(decval).toString(16) : "";
 
-    //Update Decimal Field
-    document.getElementById("decField").value = document.getElementById("octField").value;
-
-
-    //Update Binary Field
-    document.getElementById("binField").value = document.getElementById("octField").value;
-
-
-    //Update Hex Field
-    document.getElementById("hexField").value = document.getElementById("octField").value;
-
-
-    //Update ASCII Field
-    document.getElementById("asciiField").value = document.getElementById("octField").value;
-
-
+  //Update ASCII Field
+  document.getElementById("asciiField").value = document.getElementById("decField").value.length > 0 ? String.fromCharCode(Number(document.getElementById("decField").value)) : "";
 }
 
 function updateFromHex() {
-    //Constraints (only characters 0-F)
+  //Constraint (only characters 0-F)
+  let hexval = document.getElementById("hexField").value.replace(/[^0-9A-Fa-f]/, "");
+  document.getElementById("hexField").value = hexval;
 
+  //Update Decimal Field
+  let decval = parseInt(hexval,16);
+  document.getElementById("decField").value = document.getElementById("hexField").value.length > 0 ? decval : "";
 
+  //Update Binary Field
+  document.getElementById("binField").value = document.getElementById("decField").value.length > 0 ? Number(decval).toString(2) : "";
 
+  //Update Octal Field
+  document.getElementById("octField").value = document.getElementById("decField").value.length > 0 ? Number(decval).toString(8) : "";
 
-    //Update Decimal Field
-    document.getElementById("decField").value = document.getElementById("hexField").value;
-
-
-    //Update Binary Field
-    document.getElementById("binField").value = document.getElementById("hexField").value;
-
-
-    //Update Octal Field
-    document.getElementById("octField").value = document.getElementById("hexField").value;
-
-
-    //Update ASCII Field
-    document.getElementById("asciiField").value = document.getElementById("hexField").value;
-
-
+  //Update ASCII Field
+  document.getElementById("asciiField").value = document.getElementById("decField").value.length > 0 ? String.fromCharCode(Number(document.getElementById("decField").value)) : "";
 }
 
 function updateFromASCII() {
-    //Constraints (only allow input to be 1 single legal character in ASCII table)
+  //Constraint (only allow input to be 1 single legal character in ASCII table)
+  let asciival = document.getElementById("asciiField").value;
+  if (asciival.length > 1) {
+    asciival = asciival[0];
+  }
+  else if (asciival.length == 0) {
+
+  }
+  document.getElementById("asciiField").value = asciival;
 
 
 
-
-    //Update Decimal Field
-    document.getElementById("decField").value = document.getElementById("asciiField").value;
-
-
-    //Update Binary Field
-    document.getElementById("binField").value = document.getElementById("asciiField").value;
+  //Update Decimal Field
+  document.getElementById("decField").value = asciiCodes[document.getElementById("asciiField").value]
 
 
-    //Update Octal Field
-    document.getElementById("octField").value = document.getElementById("asciiField").value;
+  //Update Binary Field
+  document.getElementById("binField").value = document.getElementById("asciiField").value;
 
 
-    //Update Hex Field
-    document.getElementById("hexField").value = document.getElementById("asciiField").value;
+  //Update Octal Field
+  document.getElementById("octField").value = document.getElementById("asciiField").value;
 
 
+  //Update Hex Field
+  document.getElementById("hexField").value = document.getElementById("asciiField").value;
 }
 
 document.getElementById("toTextConButton").addEventListener("click", toTextConverter);
